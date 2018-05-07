c8ceee28beba5724e1bd8d0bcf3e9bbdc0bc8c82
ahlinder <38750135+ahlinder@users.noreply.github.com>
2018-05-07 17:42:45 +0200

    Add files via upload

---
+++ Pong/Pong/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Pong
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.spelplan = new System.Windows.Forms.Panel();
+            this.rack = new System.Windows.Forms.PictureBox();
+            this.boll = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.spelplan.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.rack)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.boll)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // spelplan
+            // 
+            this.spelplan.Controls.Add(this.rack);
+            this.spelplan.Controls.Add(this.boll);
+            this.spelplan.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.spelplan.Location = new System.Drawing.Point(0, 0);
+            this.spelplan.Name = "spelplan";
+            this.spelplan.Size = new System.Drawing.Size(402, 338);
+            this.spelplan.TabIndex = 0;
+            // 
+            // rack
+            // 
+            this.rack.BackColor = System.Drawing.Color.Red;
+            this.rack.Location = new System.Drawing.Point(144, 276);
+            this.rack.Name = "rack";
+            this.rack.Size = new System.Drawing.Size(140, 20);
+            this.rack.TabIndex = 1;
+            this.rack.TabStop = false;
+            // 
+            // boll
+            // 
+            this.boll.BackColor = System.Drawing.Color.Blue;
+            this.boll.Location = new System.Drawing.Point(215, 142);
+            this.boll.Name = "boll";
+            this.boll.Size = new System.Drawing.Size(25, 25);
+            this.boll.TabIndex = 0;
+            this.boll.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(402, 338);
+            this.Controls.Add(this.spelplan);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.spelplan.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.rack)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.boll)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel spelplan;
+        private System.Windows.Forms.PictureBox rack;
+        private System.Windows.Forms.PictureBox boll;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Pong/Pong/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pong
+{
+    public partial class Form1 : Form
+    {
+        public int fart_horisontell = 4;
+        public int fart_topp = 4;
+        public int poäng = 0;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            timer1.Enabled = true;
+            Cursor.Hide();
+
+            this.FormBorderStyle = FormBorderStyle.None; //Tar bort alla ramar så att spelet ser mer "clean" ut.           
+            this.Bounds = Screen.PrimaryScreen.Bounds; //Spelet är alltid i fullskärm.
+            rack.Top = spelplan.Bottom - 25; //Sätter rackets positition vid bottnen.
+
+        }
+        
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            rack.Left = Cursor.Position.X;
+
+            boll.Left += fart_horisontell; //Gör så att "bollen" rör sig från början.
+            boll.Top += fart_topp; // -II-
+
+            if (boll.Bottom >= rack.Top && boll.Bottom <= rack.Bottom && boll.Left >= rack.Left && boll.Right <= rack.Right) //Gör så att bollen får mer fart per studs, samt att den fastnar i botten då man förlorar.
+            {
+                fart_topp += 2;
+                fart_horisontell += 2;               
+                poäng += 1;
+                fart_topp = -fart_topp; //Ändrar riktningen på "bollen" så att den studsar uppåt istället för att fortsätta nedåt.
+            }
+            if (boll.Left <= spelplan.Left)
+            {
+                fart_horisontell = -fart_horisontell;
+            }
+            if (boll.Right >= spelplan.Right)
+            {
+                fart_horisontell = -fart_horisontell;
+            }
+            if(boll.Top <= spelplan.Top)
+            {
+                fart_topp = -fart_topp;            
+            }
+            if (boll.Bottom >= spelplan.Bottom)
+            {
+                timer1.Enabled = false;
+            }
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                this.Close();
+            }
+        }
+    }
+}
